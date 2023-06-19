942cd593ddf8375935cdb68052c8f285f86c8f32
heligayakwad <128084580+heligayakwad@users.noreply.github.com>
2023-06-19 03:24:11 +0000

    format

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,7 +6,7 @@
     static class Program
     {
 
-        static void Main()
+        static void Main()//
         {
             System.Console.WriteLine("Hello World");
             TreasureChest chest = new TreasureChest(TreasureChest.Material.Oak, TreasureChest.LockType.Intermediate, TreasureChest.LootQuality.Green);
