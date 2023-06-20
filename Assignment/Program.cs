2507aea19b136703bfae9972519c10f0e05f5adc
heligayakwad <128084580+heligayakwad@users.noreply.github.com>
2023-06-20 03:48:15 +0000

    format

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,11 +5,18 @@
     /// </summary>
     static class Program
     {
-
-        static void Main()//
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        static void Main()
         {
+            // Display a greeting message
             System.Console.WriteLine("Hello World");
+
+            // Create a new treasure chest with specific properties
             TreasureChest chest = new TreasureChest(TreasureChest.Material.Oak, TreasureChest.LockType.Intermediate, TreasureChest.LootQuality.Green);
+
+            // Display the details of the treasure chest
             Console.WriteLine(chest);
         }
     }
